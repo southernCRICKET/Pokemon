9cb52c882c709d86334dbb3349d9b99fd33852a6
Phillip <phillip.thamm@yahoo.com>
2022-09-30 13:55:22 -0500

    Json creation working. Need to update format, possible new class

--- src/Typings/CreateJson.java
+++ src/Typings/CreateJson.java
@@ -1,23 +1,39 @@
 package Typings;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
-import org.json.simple.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class CreateJson {
     public static void main(String[] args){
         HashMap<String,Type> listOfTypes = new HashMap<>();
+        String json="";
 
         for (String s: Type.STRTYPES) {
             Type t = new Type(s);
-            JSONObject jo = new JSONObject();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
             t.LoadType(t);
             listOfTypes.put(s,t);
-            jo.put("name", s);
-            Map m = new LinkedHashMap(2);
+            json += gson.toJson(t)+"\n";
+        }
 
+        try{
+            File file = new File("Types.txt");
+            if(file.createNewFile())
+                System.out.println("File Created");
+            else
+                System.out.println("File already exists");
+            FileOutputStream fos = new FileOutputStream("Types.txt");
+            fos.write(json.getBytes());
+            fos.close();
+
+        } catch (IOException e){
+            System.out.println("Error");
+            e.printStackTrace();
         }
     }
 }
